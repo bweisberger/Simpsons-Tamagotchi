70e7df743e18743e079c5ab0262c06d167d4a7bb
William Weisberger <bill.weisberger@gmail.com>
2019-07-08 14:53:35 -0600

    Added audio files, and too much other stuff

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 class Tamagotchi{
   constructor(name){
     this.name = name;
-    this.hunger = 0;
-    this.boredom = 0;
-    this.sleepiness = 0;
-    this.age = 0;
-    this.image = ''
+    this.hunger = 2;
+    this.boredom = 2;
+    this.sleepiness = 2;
+    this.age = 3;
+    this.image = 'images/homer butt scratch.gif'
   }
 
 }
@@ -16,58 +16,60 @@ const game = {
   $hunger: $('#hunger'),
   $sleepiness: $('#sleepiness'),
   $boredom: $('#boredom'),
-  // $tamagotchiImg: $('#tamagotchi-img'),
   startGame: function(name){
     this.homer = new Tamagotchi(name);
-    $leftHomer = $('#homer-left');
-    $rightHomer = $('#homer-right');
-    $leftHomer.hide();
     this.displayStats();
     this.startTimer();
   },
   startTimer: function(){
-    let count = 0
+    let count = 50
 
     const timer = setInterval(function(){
       count++;
-      // if(count > 2){
-      //   $leftHomer.show();
-      // }
-      // if($leftHomer.position().left < 200){
-      //   $leftHomer.hide();
-      // } else {$leftHomer.show();}
-      // if($rightHomer.position().left > 300){
-      //   $rightHomer.hide();
-      // } else {$rightHomer.show();}
-      if (count % 2 == 0){
+
+      if (count % 3 == 0){
         game.homer.hunger++
       }
-      if (count % 3 == 0){
+      if (count % 4 == 0){
         game.homer.boredom++
       }
-      if (count % 4 == 0){
+      if (count % 5 == 0){
         game.homer.sleepiness++
       }
-      if (count % 10 == 0){
+      if (count % 15 == 0){
         game.homer.age++
       }
       game.displayStats()
+      //transform homer at 60 seconds
+      if (count == 60){
+        $('#level-up-text').show();
+        $('#homergotchi').css({'width': '50vw'});
+        $('#homergotchi').attr('src', 'images/transforming homer.gif');
+        setTimeout(function(){
+          game.homer.image = 'images/spin on the floor homer.gif';
+          $('#level-up-text').hide()
+          $('#homergotchi').attr('src', 'images/spin on the floor homer.gif');
+          $('#homergotchi').css({'top': '75%', 'left': '20%', 'width': '30vw'});
+      }, 3000);
+    }
       if (game.homer.hunger == 10 || game.homer.boredom == 10 || game.homer.sleepiness == 10){
-        console.log(`Game over, something reached 10.`)
+        alert("Bummer. Something reached 10.")
+        location.reload();
         clearInterval(timer);
       }
     }, 1000);
   },
   displayStats: function(){
+
     $('#age').text(`${this.homer.age} hours free`);
     $('#name').text(`${this.homer.name} Simpson`);
     $('#hunger').text(`Hunger ${this.homer.hunger}`);
     $('#sleepiness').text(`Sleepiness ${this.homer.sleepiness}`);
     $('#boredom').text(`Boredom ${this.homer.boredom}`);
-
   },
   showGame: function(boolean){
     if (boolean) {
+      $('#play-game-audio')[0].play();
       $('#age').show();
       $('#name').show();
       $('#hunger').show();
@@ -75,6 +77,9 @@ const game = {
       $('#boredom').show();
       $('.button').show();
       $('.homer-land').show();
+      $('#simpsons-theme')[0].pause();
+      $('.prancing-homer-land').hide();
+      $('.selection-clouds').hide();
       $('body').css('background-image', 'url("images/living room adult swim.jpg")');
     } else {
       $('#age').hide();
@@ -83,7 +88,9 @@ const game = {
       $('#sleepiness').hide();
       $('#boredom').hide();
       $('.button').hide();
-      $('#homer-right').hide();
+      $('.homer-land').hide();
+      $('.input-div').hide();
+      $('.prancing-homer-land').show();
       $('body').css('background-image', 'url("images/simpsons house.png")');
     }
   },
@@ -91,31 +98,74 @@ const game = {
 }
 //hungry button - adds to sleepiness, takes away from hunger
 $('.hungry-button').click(function(e){
-  if ($(e.target).attr('id') == 'hungry-button'){
-    if (game.homer.hunger > 2){
-    game.homer.hunger -= 1;
+  if ($('#homergotchi').attr('src')== 'images/sleeping homer.gif'){
+    $('#homer-sleeping-text').show();
+  }
+  else if ($(e.target).attr('id') == 'hungry-button'){
+    if (game.homer.hunger >= 2){
+    game.homer.hunger -= 2;
     game.homer.sleepiness++;
+    $('#donut-homer').show();
+    $('#donut-audio')[0].play();
+    setTimeout(function(){
+      $('#donut-homer').hide();
+    },2000)
+    game.displayStats();
     }
   }
 });
 //bored button - adds to sleepiness, takes 2 from boredom
 $('.bored-button').click(function(e){
-  if ($(e.target).attr('id') == 'bored-button'){
-    if (game.homer.boredom > 2){
+  if ($('#homergotchi').attr('src')== 'images/sleeping homer.gif'){
+    $('#homer-sleeping-text').show();
+  }
+  else if ($(e.target).attr('id') == 'bored-button'){
+    if (game.homer.boredom >= 2){
     game.homer.boredom -= 2;
     game.homer.sleepiness++;
+    $('#woohoo-homer').show();
+    $('#woohoo-audio')[0].play();
+    setTimeout(function(){
+      $('#woohoo-homer').hide();
+    },2000)
+    game.displayStats();
     }
   }
 });
-//sleep button decrements sleepiness by 3, increments hunger & boredom by 1
+//sleep button decrements sleepiness by 5
 $('.sleepy-button').click(function(e){
   if ($(e.target).attr('id') == 'sleepy-button'){
-    if (game.homer.sleepiness > 3){
-    game.homer.sleepiness -= 3;
-    game.homer.boredom++;
+    if (game.homer.sleepiness >= 5){
+    game.homer.sleepiness -= 5;
+    game.displayStats();
+    $('#homergotchi').attr('src', 'images/sleeping homer.gif');
+    $('#homergotchi').css({'top': '50%', 'left':'28%'})
+    $('#sleepy-audio').play();
+    const sleepTimer = setTimeout(function(){
+      $('#sleepy-audio').pause();
+      $('#homer-sleeping-text').hide();
+      $('#homergotchi').attr('src', game.homer.image);
+
+      if (game.homer.image == 'images/homer butt scratch.gif'){
+        $('#homergotchi').css({'top': '18%', 'left': '23%'})
+      }
+      else if (game.homer.image == 'images/spin on the floor homer.gif') {
+        $('#homergotchi').css({'top': '75%', 'left': '20%', 'width': '30vw'});
+      }
+    }, 5000)
     }
     //check if method works
   }
+});
+
+$('.instructions-text').on('click', function(e){
+  console.log(e.target, 'click worked')
+  $('#instructions-modal').modal('toggle');
+});
+
+$('.start-game-text').on('click', function(){
+  $('.input-div').show();
+  $('.selection-clouds').hide();
 });
 //when the document is ready
 $(document).ready(function(){
@@ -127,6 +177,7 @@ $(document).ready(function(){
       this.value = '';
     }
   });
+  document.querySelector('#simpsons-theme').play();
   game.showGame(false);
   $('#name-button').click(function(e){
     const name = $('#name-input').val();
